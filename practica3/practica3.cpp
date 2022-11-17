f4d163384eda07b0eafe8e68ac7443d21bea9017
Jorge D <jorge1439@hotmail.com>
2022-11-17 21:40:07 +0100

    añado practica 3

--- practica3/practica3.cpp
+++ practica3/practica3.cpp
@@ -7,4 +7,30 @@
 * DNI: #76439433#
 * EMAIL: #jdaza32@alumno.uned.es#
 ***************************************/
+int main() {
 
+/*
+1º1 enero 1601 fue lunes
+2ºbisiestos multiplos 4 menos multiplos de 100 y 400
+3ºfuncion saber si el año es bisiesto
+4ºdia de la semana que comienza un mes de un año
+Tipos enumerados para dias semana
+imprimir pantalla usando funciones
+
+
+
+
+
+*/
+  int month;
+  int year;
+
+  printf("¿Mes (1..12?");
+  scanf("%d",&month);
+  printf("\n");
+  printf("¿Ano (1601..3000)?");
+  scanf("%d",&year);
+
+
+  return 0;
+}
