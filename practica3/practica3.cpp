c718d3c5d2a5a30c65e41703a007aa84768e7e1c
Jorge D <jorge1439@hotmail.com>
2022-11-18 13:32:48 +0100

    actualizacion

--- practica3/practica3.cpp
+++ practica3/practica3.cpp
@@ -7,8 +7,6 @@
 * DNI: #76439433#
 * EMAIL: #jdaza32@alumno.uned.es#
 ***************************************/
-int main() {
-
 /*
 1º1 enero 1601 fue lunes
 2ºbisiestos multiplos 4 menos multiplos de 100 y 400
@@ -16,14 +14,19 @@ int main() {
 4ºdia de la semana que comienza un mes de un año
 Tipos enumerados para dias semana
 imprimir pantalla usando funciones
-
-
-
-
-
 */
-  typedef enum Months{ENERO,FEBRERO,MARZO,ABRIL,MAYO,JUNIO,JULIO,AGOSTO,SEPTIEMBRE,OCTUBRE,NOVIEMBRE,DICIEMBRE};
-  typedef enum Days{LU,MA,MI,JU,VI,SA,DO};
+
+
+typedef enum Months{ENERO,FEBRERO,MARZO,ABRIL,MAYO,JUNIO,JULIO,AGOSTO,SEPTIEMBRE,OCTUBRE,NOVIEMBRE,DICIEMBRE};
+typedef enum Days{LU,MA,MI,JU,VI,SA,DO};
+
+
+
+
+int main() {
+
+
+
   int month;
   int year;
   char equal="=";
